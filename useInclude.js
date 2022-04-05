e30284cfd62c99a573123c5fe2fca7fb8f08f00a
Deepti Singh <deepti.m.singh@gmail.com>
2022-04-05 13:17:38 +0530

    includes and Object methods like keys ,values,enteries

---
+++ useInclude.js
@@ -0,0 +1,7 @@
+//includes method is used for both string and array
+str=" a happy world ";
+arr=["mango","banana","apple"];
+console.log(str.includes("world"));
+console.log(arr.includes("mango"));
+//syntax include("search string",starting position)
+console.log(arr.includes("mango" ,1)); // 1 is starting index
---
+++ useObjectEnteries.js
@@ -0,0 +1,24 @@
+const users = {
+    '2345234': {
+      name: "John",
+      age: 29
+    },
+    '8798129': {
+      name: "Jane",
+      age: 42
+    },
+    '1092384': {
+      name: "Fred",
+      age: 17 
+    }
+  };
+   console.log(Object.entries(users));
+  const usersOver20 = Object.entries(users).reduce((acc, [id, user]) => {   // we are doing array destuctring of curr as Object.enteries gives output of array of arraya of key and value
+    if (user.age > 20) {
+      acc.push({ ...user, id });   //pushing an object in empty array which has name,age,id
+    }  
+    return acc;
+  }, []);
+  console.log(usersOver20);
+
+  //find the data with age age above 20...of comples object stucture above
