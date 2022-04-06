1ae0b5fb6035d4f08553feba8ccece9e36e5c10c
Deepti Singh <deepti.m.singh@gmail.com>
2022-04-06 13:20:17 +0530

    Update async-await.js
    aync await practise

--- async-await.js
+++ async-await.js
@@ -1,4 +1,4 @@
-//import axios from 'axios';
+
 async function get_data()
 {
     const response = await fetch('https://api.github.com/users/1');
@@ -7,9 +7,49 @@ async function get_data()
     console.log(data);
      return data;
 } 
-//console.log("i am using axios");
+
 get_data().then(()=>{console.log(data);})
 //let a=get_data();
 //console.log(a);
 
-// need to debug the same. 
+ 
+
+
+//in normal async function whatever value we return is handled by .then as a resolved value.
+async function getBlogPost() {
+    return 'works here too!';  
+  }
+  
+  getBlogPost().then(value => console.log(value));
+
+//here value is handled by .then which is returned by async. Async always return a promise 
+
+//previously have to write then 2 times but with async await we pause the code until promise is resolved
+// const response = fetch('https://jsonplaceholder.typicode.com/posts/1');
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+async function getPost() {
+    const response=await fetch('https://jsonplaceholder.typicode.com/posts/1') ; 
+    const data= await response.json();
+    console.log(data);
+  }
+  getPost();
+
+
+  //handling errors with try catch 
+
+  async function getPerson(){
+    try{
+    const response=await fetch('https://jsonplaceholder.typicode.com/users/3');
+    if (!response.ok) {
+          throw new Error(response.status);  
+        }
+    const data=await response.json();
+    console.log(`${data.name} works for ${data.company.name}`)
+    }catch(error){
+      console.log(error)
+    }
+    
+  }
+  
+  getPerson();
